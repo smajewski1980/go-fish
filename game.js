aa254da7e4cd260f7873e1cd719f4ced6c54af0b
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-03-10 20:58:33 -0400

    set up players in game obj,set up current player and switch method,outlined take turn method

--- game.js
+++ game.js
@@ -1,14 +1,52 @@
+let currentPlayer = 1;
+let oppositePlayer;
+currentPlayer === 1 ? (oppositePlayer = 0) : (oppositePlayer = 1);
+
 class Game {
   constructor() {
     this.currentDeck = new DeckOfCards();
-    this.handOne = [];
-    this.handTwo = [];
+    this.playerOne = {
+      name: "player one",
+      hand: [],
+      books: [],
+    };
+    this.playerTwo = {
+      name: "player two",
+      hand: [],
+      books: [],
+    };
+    this.players = [this.playerOne, this.playerTwo];
+  }
+
+  switchCurrentPlayer() {
+    currentPlayer === 1 ? (currentPlayer = 0) : (currentPlayer = 1);
+  }
+
+  welcome() {
+    confirm(`The computer will be player 1, push ok to start.`);
+  }
+
+  takeTurn(player) {
+    // display players hand
+    // have player select card to ask for
+    // if computer player, ask for random card
+    // see if that card is in opposite players hand
+    // if not draw from pile
+    // if so, transfer to other players hand
+    // check for book
+    // if book add book
+    this.switchCurrentPlayer();
+  }
+
+  playGame() {
+    this.welcome();
+    this.takeTurn(this.players[currentPlayer]);
   }
 
   deal() {
     for (let i = 1; i < 6; i++) {
-      this.handOne.push(this.currentDeck.shuffledDeck.pop());
-      this.handTwo.push(this.currentDeck.shuffledDeck.pop());
+      this.playerOne.hand.push(this.currentDeck.shuffledDeck.pop());
+      this.playerTwo.hand.push(this.currentDeck.shuffledDeck.pop());
     }
   }
 
@@ -17,11 +55,12 @@ class Game {
   }
 
   printHands() {
-    console.log(this.handOne);
-    console.log(this.handTwo);
+    console.log(this.playerOne.hand);
+    console.log(this.playerTwo.hand);
   }
 }
 
 const currentGame = new Game();
 currentGame.deal();
-currentGame.printHands();
+currentGame.playGame();
+// currentGame.printHands();
